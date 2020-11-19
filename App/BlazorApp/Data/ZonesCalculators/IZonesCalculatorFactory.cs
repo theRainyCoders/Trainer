2b5e388406537f33c2fdba9a8027d9d86f803107
Joerg Bernhard <bernhard.joerg@googlemail.com>
2020-11-19 15:27:16 +0100

    added a factory that provides and create all available zones calculators they are available in the assembly

---
+++ App/BlazorApp/Data/ZonesCalculators/IZonesCalculatorFactory.cs
@@ -0,0 +1,30 @@
+﻿namespace TheRainyCoders.Trainer.BlazorApp.Data.ZonesCalculators
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    ///     Interface that describes a factory for creating <see cref="IZonesCalculator" />.
+    /// </summary>
+    internal interface IZonesCalculatorFactory
+    {
+        /// <summary>
+        ///     Get the description of all available zone calculators.
+        /// </summary>
+        /// <returns>
+        ///     The <see cref="IReadOnlyCollection{ZoonesCalculatorDescription}" />.
+        /// </returns>
+        IEnumerable<ZonesCalculatorDescription> GetAvailableCalculators();
+
+        /// <summary>
+        ///     Creates a instance of a zone calculator.
+        /// </summary>
+        /// <param name="id">
+        ///     The unique id of the zone calculator that should be created.
+        /// </param>
+        /// <returns>
+        ///     The <see cref="IZonesCalculator" />.
+        /// </returns>
+        IZonesCalculator Create(Guid id);
+    }
+}
---
+++ App/BlazorApp/Data/ZonesCalculators/ZonesCalculatorFactory.cs
@@ -0,0 +1,62 @@
+﻿namespace TheRainyCoders.Trainer.BlazorApp.Data.ZonesCalculators
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    ///     Factory to create instances of the available <see cref="IZonesCalculator" />.
+    /// </summary>
+    internal class ZonesCalculatorFactory : IZonesCalculatorFactory
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="ZonesCalculatorFactory" /> class.
+        /// </summary>
+        public ZonesCalculatorFactory()
+        {
+            this.Calculators = GetAvailableZoneCalculatorsOfThisAssembly();
+        }
+
+        /// <summary>
+        ///     Gets available calculators.
+        /// </summary>
+        protected Dictionary<Guid, IZonesCalculator> Calculators { get; }
+
+        /// <inheritdoc />
+        public virtual IEnumerable<ZonesCalculatorDescription> GetAvailableCalculators()
+        {
+            return this.Calculators.Values.Select(x => x.Description);
+        }
+
+        /// <inheritdoc />
+        public virtual IZonesCalculator Create(Guid id)
+        {
+            return this.Calculators[id];
+        }
+
+        /// <summary>
+        ///     Get available zone calculators of this assembly via reflection.
+        /// </summary>
+        /// <returns>
+        ///     The <see cref="Dictionary{Guid, IZonesCalculator}" />.
+        /// </returns>
+        private static Dictionary<Guid, IZonesCalculator> GetAvailableZoneCalculatorsOfThisAssembly()
+        {
+            var calculators = new Dictionary<Guid, IZonesCalculator>();
+
+            var type = typeof(IZonesCalculator);
+            var calculatorTypes = AppDomain.CurrentDomain.GetAssemblies().SelectMany(s => s.GetTypes())
+                .Where(p => p.IsClass && type.IsAssignableFrom(p));
+
+            foreach (var calculatorType in calculatorTypes)
+            {
+                if (Activator.CreateInstance(calculatorType) is IZonesCalculator calculator)
+                {
+                    calculators[calculator.Description.Id] = calculator;
+                }
+            }
+
+            return calculators;
+        }
+    }
+}
