04c4b1e88ff2ee597627458122ca4dc22889d1fd
Joerg Bernhard <bernhard.joerg@googlemail.com>
2020-11-22 15:07:13 +0100

    added localization for web pages

--- App/BlazorApp/Startup.cs
+++ App/BlazorApp/Startup.cs
@@ -1,5 +1,8 @@
 namespace TheRainyCoders.Trainer.BlazorApp
 {
+    using System.Collections.Generic;
+    using System.Globalization;
+
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.Configuration;
@@ -8,25 +11,60 @@ namespace TheRainyCoders.Trainer.BlazorApp
 
     using TheRainyCoders.Trainer.BlazorApp.Data;
 
+    /// <summary>
+    ///     Startup configuration of the web application.
+    /// </summary>
     public class Startup
     {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="Startup" /> class.
+        /// </summary>
+        /// <param name="configuration">
+        ///     The configuration.
+        /// </param>
         public Startup(IConfiguration configuration)
         {
             this.Configuration = configuration;
         }
 
+        /// <summary>
+        ///     Gets the configuration.
+        /// </summary>
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        /// <summary>
+        ///     This method gets called by the runtime. Use this method to add services to the container.
+        ///     For more information on how to configure your application, visit <![CDATA[ https://go.microsoft.com/fwlink/?LinkID=398940 ]]>.
+        /// </summary>
+        /// <param name="services">
+        ///     The services.
+        /// </param>
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+            var supportedCultures = new List<CultureInfo> { new CultureInfo("en"), new CultureInfo("de") };
+            services.Configure<RequestLocalizationOptions>(
+                options =>
+                    {
+                        options.DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture("en");
+                        options.SupportedUICultures = supportedCultures;
+                    });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        ///     This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app">
+        ///     The app.
+        /// </param>
+        /// <param name="env">
+        ///     The env.
+        /// </param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -41,6 +79,7 @@ namespace TheRainyCoders.Trainer.BlazorApp
                 app.UseHsts();
             }
 
+            app.UseRequestLocalization();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
